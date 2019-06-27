b351b50bcd0dd6ca72f3773e84e22d8b08831dfc
Alexander Heck <alexanderheck@Alexanders-MacBook-Pro.local>
2019-06-27 15:45:35 +0200

    server update 5

--- server/main.js
+++ server/main.js
@@ -10,12 +10,42 @@ app.get("/", (req, res) => {
 
 app.listen(4000);
 
-lists: {
-  1: ["A","B","C","D","E","F"],
-  2: ["A","B","C","D","E","F"],
-  3: ["A","B","C","D","E","F"],
-},
-short_description: "Du bist hier, weil du eine Idee brauchst. Mal sehen was dir so einfällt.",
-long_description: "Dies ist eine Ideengenerierung die auf dem Prinzip der Semantischen Intuition basiert. Nutze die Knöpfe die dir gegeben sind um die das Erlebnis anzupassen.",
-saved_combinations: [],
-idea_description: "Hier steht gleich deine super Idee."
+// data
+const lists = {
+  1: ["A", "B", "C", "D", "E", "F"],
+  2: ["A", "B", "C", "D", "E", "F"],
+  3: ["A", "B", "C", "D", "E", "F"]
+};
+
+const text = {
+  short_description:
+    "Du bist hier, weil du eine Idee brauchst. Mal sehen was dir so einfällt.",
+  long_description:
+    "Dies ist eine Ideengenerierung die auf dem Prinzip der Semantischen Intuition basiert. Nutze die Knöpfe die dir gegeben sind um die das Erlebnis anzupassen.",
+  saved_combinations: [],
+  idea_description: "Hier steht gleich deine super Idee."
+};
+
+// index route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    msg: "Server Test"
+  });
+});
+
+// list routes
+app.get("/lists", (req, res) => {
+  res.status(200).json({
+    lists
+  });
+});
+// text routes
+app.get("/text", (req, res) => {
+  res.status(200).json({
+    text
+  });
+});
+
+app.listen(4000, () => {
+  console.log("server: http://localhost:4000");
+});
