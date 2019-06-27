ef52cea3f61395628559916214f877242e4b9ffa
Alexander Heck <alexanderheck@Alexanders-MacBook-Pro.local>
2019-06-27 15:22:22 +0200

    server update 4

--- server/main.js
+++ server/main.js
@@ -9,3 +9,13 @@ app.get("/", (req, res) => {
 })
 
 app.listen(4000);
+
+lists: {
+  1: ["A","B","C","D","E","F"],
+  2: ["A","B","C","D","E","F"],
+  3: ["A","B","C","D","E","F"],
+},
+short_description: "Du bist hier, weil du eine Idee brauchst. Mal sehen was dir so einfällt.",
+long_description: "Dies ist eine Ideengenerierung die auf dem Prinzip der Semantischen Intuition basiert. Nutze die Knöpfe die dir gegeben sind um die das Erlebnis anzupassen.",
+saved_combinations: [],
+idea_description: "Hier steht gleich deine super Idee."
